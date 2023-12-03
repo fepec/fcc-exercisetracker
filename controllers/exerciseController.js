124e36a9aa805d79f213bd9aedaf10ccb8126260
fepec <felipe.estrada.prada@gmail.com>
2023-12-02 22:59:44 -0500

    Add Exercise POST

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -3,7 +3,21 @@ const asyncHandler = require("express-async-handler");
 
 // Handle exercise create on POST.
 exports.exercise_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Exercise create POST");
+    // res.send(`NOT IMPLEMENTED: Exercise detail ${req.params.id}`);
+    // TODO Validate and sanitize
+    // Create new Exercise object
+    const exercise = new Exercise({
+        user: req.params.id,
+        description: req.body.description,
+        duration: req.body.duration
+    })
+    if (req.body.date) {
+        exercise.date = req.body.date
+    }
+    // Save user
+    const savedExercise = await exercise.save();
+    res.send(savedExercise)
+
 });
 
 // Display detail page for a specific exercise.
